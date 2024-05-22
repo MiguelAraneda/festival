6efc2666191d3eedb4617f9bb49e0881231c9113
Miguel <profesor.maac@gmail.com>
2024-05-22 16:04:09 -0400

    Agregando .pipe al proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
-export function hola(){
-    console.log('Hola desde Gulpfile.js')
+import {src, dest} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+// export function hola(done){
+//     console.log('Hola desde Gulpfile.js')
+
+//     done()//finaliza la función
+// }
+
+const sass = gulpSass(dartSass)
+
+export function css( done ){
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('dist/css'))
+
+    done()
 }
